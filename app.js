e7b55deff7478e3a99378654d49f27c784247db2
Vladimir Raygel <vraygel@mail.ru>
2025-04-26 21:04:02 +0500

    Сделал портфолио

--- app.js
+++ app.js
@@ -18,6 +18,24 @@ app.use(express.json());
 app.use('/', indexRouter);
 app.use('/api', apiRouter); // Подключение маршрута api
 
+
+
+// Обслуживание статических файлов из папки list-products
+app.use('/list-products', express.static(path.join(__dirname, 'public', 'portfolio', 'list-products')));
+
+// Маршрут для перехода на страницу list-products
+app.get('/list-products', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'portfolio', 'list-products', 'index.html'));
+});
+
+// Обработка ошибок 404
+app.use((req, res) => {
+  res.status(404).send('404: Page not found');
+});
+
+
+
+
 // Обработка ошибок 404
 app.use((req, res) => {
   res.status(404).send('404: Page not found');
